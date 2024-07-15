919ab5452c4273b5356a7f3cb8994747f4bd6117
tutq <tutq@besgoup.vn>
2024-07-15 08:26:18 +0700

    a

---
+++ resources/views/auth/guideOrder.html.php
@@ -0,0 +1,51 @@
+<?php
+include dirname(__FILE__) . '/../includes/header.html.php';
+?>
+<div class="main-content">
+    <div class="container content-html-static">
+        <div class="page-title">Chính sách đặt hàng</div>
+
+        <p><span style="font-size:14px;">Để đặt hàng tại website vuaduoc.com, hãy chắc chắn rằng quý khách đã ĐĂNG KÝ TÀI KHOẢN tại vuaduoc.com</span></p>
+
+        <p><span style="font-size:14px;">Sau đó, quý khách vui lòng thực hiện theo 6 bước sau:</span></p>
+
+        <p><span style="font-size:14px;"><strong>Bước 1:</strong> Truy cập website <a style="color:#0d6efd;" href="https://vuaduoc.com/">vuaduoc.com</a></span></p>
+
+        <p><span style="font-size:14px;"><strong>Bước 2:</strong> Click vào một trong các menu sau :</span></p>
+
+        <p><span style="font-size:14px;">"Flash sale / Thực Phẩm Chức Năng / Dược Mỹ Phẩm / Chăm Sóc Cá Nhân / Sản Phẩm Vua Dược / Thiết Bi Y Tế"</span></p>
+
+        <p><span style="font-size:14px;"><strong>Bước 3:</strong> Tìm kiếm sản phẩm cần mua ,chọn sản phẩm kèm số lượng</span></p>
+
+        <p><span style="font-size:14px;"><strong>Bước 4:</strong> Click vào "<a style="color:#0d6efd;" href="https://vuaduoc.com/cart">Xem giỏ hàng</a>"</span></p>
+
+        <p><span style="font-size:14px;"><strong>Bước 5:</strong> Kiểm tra lại đơn hàng, Thông tin nhận hàng, Ghi chú ( nếu có ) và click vào nút "Đặt hàng ngay"</span></p>
+
+        <p><span style="font-size:14px;"><strong>Bước 6:</strong> Kiểm tra lại lần cuối Thông tin nhận hàng và quét QR code để thanh toán đơn hàng</span></p>
+
+        <p><span style="font-size:14px;">Nếu quý khách hàng chưa biết cách đặt hàng vui lòng liên hệ theo số <a style="color:#0d6efd;" href="tel:0878929789">0342.342.366</a> để được hướng dẫn chi tiết.</span></p>
+
+        <p><span style="color:#FF0000;"><span style="font-size:14px;"><strong>CHÍNH SÁCH THANH TOÁN</strong></span></span></p>
+
+        <p><span style="font-size:14px;"><strong>1.  Thanh toán bằng tiền mặt</strong></span></p>
+
+        <p><span style="font-size:14px;">Thanh toán tiền mặt trực tiếp ngay khi Quý khách nhận hàng.</span></p>
+
+        <p><span style="font-size:14px;">Vua dược sẽ giao hàng tới địa chỉ mà Quý khách đã yêu cầu trong đơn hàng và nhận thanh toán trực tiếp.</span></p>
+
+        <p><span style="font-size:14px;"><strong>2. Thanh toán bằng chuyển khoản</strong></span></p>
+
+        <p><span style="font-size:14px;">Để thanh toán đơn hàng bằng hình thức chuyển khoản, Quý khách vui lòng chuyển tiền vào tài khoản bên dưới hoặc tài khoản trong quá trình đặt hàng ( quét QR code ):</span></p>
+
+        <p><span style="font-size:14px;">CÔNG TY CỔ PHẦN DỊCH VỤ THƯƠNG MẠI VÀ CÔNG NGHỆ DAK</span></p>
+
+        <p><span style="font-size:14px;">Số tài khoản : 0851000024828</span></p>
+
+        <p><span style="font-size:14px;">Tại Ngân hàng : TMCP Ngoại thương Việt Nam (Vietcombank)</span></p>
+
+        <p><span style="font-size:14px;">Sau khi chuyển tiền, Quý khách vui lòng email hoặc điện thoại cho chúng tôi theo số: <a style="color:#0d6efd;" href="tel:0878929789">0342.342.366</a>, thông báo việc chuyển tiền và số tài khoản của quý khách để tiện trong việc kiểm tra.</span></p>
+    </div>
+</div>
+<?php
+include dirname(__FILE__) . '/../includes/footer.html.php';
+?>
