0edec5dbb3edd3fbcf9b33fc6599dd661c2c7ef1
tutq <tutq@besgoup.vn>
2024-10-09 13:33:17 +0700

    add

---
+++ app/admin/import_users.php
@@ -0,0 +1,62 @@
+<?php
+require_once("bootstrap.php");
+require_once 'resource/PHPExcel/Classes/PHPExcel.php';
+
+
+$fs_filepath = "/home/oo91mah3s4eq/public_html/public/temp/vuaduoc_user.xlsx";
+$inputFileType = PHPExcel_IOFactory::identify($fs_filepath);
+$objReader = PHPExcel_IOFactory::createReader($inputFileType);
+$objPHPExcel = $objReader->load($fs_filepath);
+$allDataInSheet = $objPHPExcel->getActiveSheet()->toArray(null, true, true, true);
+$i=0;
+$j=0;
+$inserdata =array();
+$dataerr =array();
+foreach ($allDataInSheet as $value) {
+	if(empty($value['B']) || !intval($value['D'])){
+		continue;
+	}
+	$inserdata[$i]['name'] 	= $value['A'];
+        $inserdata[$i]['addres'] = $value['B'];
+	$inserdata[$i]['provice'] = $value['C'];
+        $inserdata[$i]['phone'] = $value['D'];
+	$i++;
+}
+
+$use_name = getValue('money_file_name', 'str', 'POST', '');
+for($j=0;$j<count($inserdata);$j++){
+    $phone = $inserdata[$j]['phone'];
+    $name = $inserdata[$j]['name'];
+    $name = explode("-",$name)[0];
+    $addres = $inserdata[$j]['addres']." ( ".$inserdata[$j]['provice']." )";
+    $check_user = \App\Models\Users\Users::where('use_login=\'' . $phone . '\' AND use_active != -99')->find();
+    if ($check_user) {
+        continue;
+    }
+
+    $confirm_code = rand(1000, 9999);
+
+    $user = [
+        'use_active' => 0,
+        'use_login' => $phone,
+        'use_loginname' => $phone,
+        'use_mobile' => $phone,
+        'use_phone' => $phone,
+        'use_password' => md5($phone), // mặc định passwork là sdt đăng ký
+        'use_gender' => 1,
+        'use_email' => "",
+        'use_name' => $name,
+        'use_referral_id' => 0,
+        'use_register_confirm_code' => $confirm_code,
+        'use_avatar' => 'a4cdd34e884618b1bbf8568cfb56e082.jpg',
+        'use_province_id' => 0,
+        'use_district_id' => 0,
+        'use_ward_id' => 0,
+        'use_address_register' => $addres,
+        'use_job_code' => 1,
+        'use_referer_code'=>'',
+        'use_partner_note'=>'',
+        'use_content'=>""
+    ];
+    $user_id = \App\Models\Users\Users::insert($user);
+}
